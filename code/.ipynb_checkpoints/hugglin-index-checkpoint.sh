8a2633eb69be54aae668f6ff45a6ba7fa85ce241
Nils Ratnaweera <rata@zhaw.ch>
2022-07-12 11:29:31 +0200

    initial commit

---
+++ code/.ipynb_checkpoints/hugglin-index-checkpoint.sh
@@ -0,0 +1,6 @@
+
+cd /home/nils/zhaw-laufwerke/pools-n/N-IUNR-CONSUS-CLIMATE/CH2018
+
+find -iname "tas_*-RCP*_QM_1981-2099_10-229_2-102_mask.nc" -exec cp {} /home/nils/ownCloud/Projekte/2022_02_PiWi/data/ \;
+
+find -iname "tasmax_*-RCP*_QM_1981-2099_10-229_2-102_mask.nc" -exec cp {} /home/nils/ownCloud/Projekte/2022_02_PiWi/data/ \;
